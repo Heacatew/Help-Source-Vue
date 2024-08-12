f9c6b8974e90c81e180c995dc548d052d274481d
zhangjia <a2556714286@gmail.com>
2024-08-12 18:32:03 +0800

    变更

--- source-common/src/main/java/cn/source/common/utils/sms/DySmsUtil.java
+++ source-common/src/main/java/cn/source/common/utils/sms/DySmsUtil.java
@@ -45,30 +45,6 @@ public class DySmsUtil {
         boolean result = true;
         String url = "sms.tencentcloudapi.com";
         try {
-            //实例化一个Credential，也就是认证对象，参数是密钥对；你要使用肯定要进行认证
-            Credential credential = new Credential("AKIDxkIZaK2UfYAVaOvBAtLWQ42oMfiW4MOA", "XpsfCTVHo58vSjkV0GG1WlkV0GH3artf");
-            //HttpProfile这是http的配置文件操作，比如设置请求类型(post,get)或者设置超时时间了、还有指定域名了
-            HttpProfile httpProfile = new HttpProfile();
-            //这个setEndpoint可以省略的
-            httpProfile.setEndpoint(url);
-            //实例化一个客户端配置对象,这个配置可以进行签名（使用私钥进行加密的过程），对方可以利用公钥进行解密
-            ClientProfile clientProfile = new ClientProfile();
-            clientProfile.setHttpProfile(httpProfile);
-            //实例化要请求产品(以sms为例)的client对象
-            SmsClient smsClient = new SmsClient(credential, "ap-beijing", clientProfile);
-            //实例化request封装请求信息
-            SendSmsRequest request = new SendSmsRequest();
-            String[] phoneNumber = {"+86"+phone};
-            request.setPhoneNumberSet(phoneNumber);     //设置手机号
-            request.setSmsSdkAppid("1400643913");
-            request.setSign("野生地瓜公众号");
-            request.setTemplateID("1762220");
-            String verificationCode = paramJson.get("code").toString();
-            String[] templateParamSet = {verificationCode};
-            request.setTemplateParamSet(templateParamSet);
-            //发送短信
-            SendSmsResponse response = smsClient.SendSms(request);
-            logger.info(SendSmsResponse.toJsonString(response));
 
         }catch (Exception e){
             result = false;
@@ -79,7 +55,7 @@ public class DySmsUtil {
 
     public static void main(String[] args) {
         JSONObject obj = new JSONObject();
-        obj.put("code", "1234");
-        sendSms("19953642212", obj,null,null,null);
+        obj.put("co1de", "1");
+        sendSms("1", obj,null,null,null);
     }
 }
